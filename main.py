79ac77b943d6a99cda1e91363a286440177e256b
Brandon Mulcahy <brandon@jangler.info>
2015-04-25 22:25:21 -0400

    Send text from textin to textout

--- main.py
+++ main.py
@@ -29,13 +29,16 @@ class Application(tk.Frame):
         self.status.pack(side='bottom', fill='x')
 
         self.textin = tk.Text(self)
-        self.textin['height'] = tkinter.font.nametofont(
-                self.textin['font'])['size']
         self.textin['height'] = 0
+        self.textin.bind('<Return>', self.sendtext)
         self.textin.pack(side='bottom', fill='x')
 
         self.textout = tkinter.scrolledtext.ScrolledText(self)
         self.textout.pack(side='bottom', expand=1, fill='both')
+
+    def sendtext(self, event):
+        self.textout.insert('end', self.textin.get('1.0', 'end')[:-1])
+        self.textin.delete('1.0', 'end')
 
 
 root = tk.Tk()
